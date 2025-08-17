7ede077bbbea9595f4d45a00f32dc32ba3a4cfb7
shtsukada <st01251988@gmail.com>
2025-08-17 15:34:15 +0900

    serverMain

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,68 @@
 package main
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/shtsukada/cloudnative-observability-app/pkg/observability"
+)
+
+const (
+	grpcAddr    = ":8080"
+	metricsAddr = ":9090"
+)
+
+func main() {
+	logger := observability.NewLogger()
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	metricsSrv := &http.Server{Addr: metricsAddr, Handler: mux}
+
+	grpcLis, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		logger.Fatal("failed to listen", "addr", grpcAddr, "err", err)
+	}
+	grpcSrv := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(observability.UnaryLoggingInterceptor(logger)),
+	)
+	healthSrv := health.NewServer()
+	healthpb.RegisterHealthServer(grpcSrv, healthSrv)
+	reflection.Register(grpcSrv)
+
+	go func() {
+		logger.Info("metrics http starting", "addr", metricsAddr)
+		if err := metricsSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Error("metrics http error", "err", err)
+		}
+	}()
+	go func() {
+		logger.Info("grpc starting", "addr", grpcAddr)
+		if err := grpcSrv.Serve(grpcLis); err != nil {
+			logger.Error("grpc serve error", "err", err)
+		}
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
+	logger.Info("shutting down...")
+
+	grpcSrv.GracefulStop()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_ = metricsSrv.Shutdown(ctx)
+
+	logger.Info("bye")
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,18 @@ module github.com/shtsukada/cloudnative-observability-app
 go 1.24.5
 
 require (
+	github.com/prometheus/client_golang v1.23.0
 	go.uber.org/zap v1.27.0
 	google.golang.org/grpc v1.74.2
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.65.0 // indirect
+	github.com/prometheus/procfs v0.16.1 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
